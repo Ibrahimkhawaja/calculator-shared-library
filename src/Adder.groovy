4446f265958c2c139409458af21213fb7a252063
Ahsan ud din <auddin@nisum.com>
2022-06-30 16:09:10 +0500

    correcting multiply

--- src/Adder.groovy
+++ src/Adder.groovy
@@ -2,14 +2,4 @@ class Adder {
     def Add(int x, int y) {
         return x+y
     }
-    def Subtract(int x, int y) {
-        return x-y
-    }
-    def Multiply(int x, int y){
-        return x*y
-    }
-    def Divide(int x, int y)
-    {
-        return x/y
-    }
 }
