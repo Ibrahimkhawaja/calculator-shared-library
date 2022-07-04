5d002a7a488f7cb05d27fbe7b5933119b95c3e6c
ahmedghilman <40575070+ahmedghilman@users.noreply.github.com>
2022-07-04 13:56:12 +0500

    Update Divider.groovy

--- src/Divider.groovy
+++ src/Divider.groovy
@@ -1,10 +1,10 @@
 class Divider {
-    def Divide(int x, int y) {
-    try {
+    def Divide (int x, int y) {
+ //   try {
          return x/y
-    }
-    catch (ArithmeticException e) {
-      System.out.println(e.getMessage())
-    }
+//    }
+//    catch (ArithmeticException e) {
+//      System.out.println(e.getMessage())
+//    }
 }
 }
