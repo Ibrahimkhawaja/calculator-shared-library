f274dd3966337df75a588fb73db0961fa0d103fe
ahmedghilman <40575070+ahmedghilman@users.noreply.github.com>
2022-07-04 12:07:18 +0500

    Create Divide.groovy

---
+++ src/Divide.groovy
@@ -0,0 +1,11 @@
+class Adder {
+  try { def Divide(int x, int y) {
+        return x/y
+    }
+      }
+  catch (ArithmeticException e) {
+            // Exception handler
+            SSystem.out.println(e.getMessage());
+        }
+    }
+}
